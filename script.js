33cc2c880affb816908c76970ea32b8bebd2eeb3
Your Name <you@example.com>
2020-11-09 11:12:07 +0330

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function respMenuFunc() {
+    var x = document.getElementById("headerTopnav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } 
+    else {
+        x.className = "nav";
+        }
+    }
