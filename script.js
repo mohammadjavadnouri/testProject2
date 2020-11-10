5c4f64c9a51f77dde234bc9abbb793ac2548b358
Your Name <you@example.com>
2020-11-10 15:45:56 +0330

    improve responsiveness of 3th and 4th row of the main page

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function respMenuFunc() {
-    var x = document.getElementById("headerTopnav");
-    if (x.className === "nav") {
-        x.className += " responsive";
-    } 
-    else {
-        x.className = "nav";
-        }
-    }
+  var x = document.getElementById("headerTopnav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
